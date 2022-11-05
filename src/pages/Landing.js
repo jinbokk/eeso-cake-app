865fba5d4b26e27ea3bdb36f425cd5c972e5ff99
JINBOK LEE <eyelash1024@naver.com>
2022-11-05 23:47:18 +0900

    landing page code rewriting

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -10,7 +10,7 @@ import "./css/Landing.css";
 // gsap test -----------------------------------------
 // https://greensock.com/react/
 import { gsap } from "gsap";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 // gsap test -----------------------------------------
 
 const Landing = (props) => {
@@ -73,96 +73,102 @@ const Landing = (props) => {
 
   return (
     <>
-     <Container className="landing_container_top" ref={el}>
-          <div className="landing_text">
-            <div className="landing_text_section1">
-              <h2 className="ease_in_left_1">Design Cake Shop</h2>
-              <h1 className="ease_in_left_2">EESO CAKE</h1>
-            </div>
-  
-            <div className="landing_text_section2">
-              <div className="ease_in_left_3">
-                <h3>안녕하세요, 이소케이크 입니다 : )</h3>
-                <h3>특별하고 소중한 날 ,</h3>
-                <h3>고객님의 행복을 위해 최선을 다하겠습니다</h3>
-              </div>
-            </div>
-  
-            <div className="landing_text_section3">
-              <button
-                className="landing_button ease_in_bottom_2"
-                onClick={() => props.setBrowse(true)}
-              >
-                SHOP NOW
-              </button>
-            </div>
-          </div>
-  
-          <div className="landing_image_container ease_in_right_1">
-            <Swiper
-              spaceBetween={500}
-              autoplay={{
-                delay: 4500,
-                disableOnInteraction: false,
-              }}
-              speed={1100}
-              modules={[Autoplay]}
-              loop={true}
-            >
-              <SwiperSlide>
-                <img
-                  src="/images/landing_test_3.png"
-                  className="landing_image_background_removed ease_in_right_2"
-                  alt=""
-                ></img>
-              </SwiperSlide>
-              <SwiperSlide>
-                <img
-                  src="/images/landing_test.png"
-                  className="landing_image_background_removed ease_in_right_2"
-                  alt=""
-                ></img>
-              </SwiperSlide>
-              <SwiperSlide>
-                <img
-                  src="/images/landing_test_2.png"
-                  className="landing_image_background_removed ease_in_right_2"
-                  alt=""
-                ></img>
-              </SwiperSlide>
-            </Swiper>
-          </div>
-  
-          {/* <div>
-                  <img
-                    src="/images/deco_cake.png"
-                    alt=""
-                    className="deco_cake_image_left ease_in_bottom_1"
-                  />
+      <Container className="landing_container_top" ref={el}>
+        <Row>
+          <Col lg={6}>
+            <Row>
+              <Col className="landing_text_section1">
+                <h2 className="ease_in_left_1">Design Cake Shop</h2>
+                <h1 className="ease_in_left_2">EESO CAKE</h1>
+              </Col>
+
+              <Col className="landing_text_section2">
+                <div className="ease_in_left_3">
+                  <h3>안녕하세요, 이소케이크 입니다 : )</h3>
+                  <h3>특별하고 소중한 날 ,</h3>
+                  <h3>고객님의 행복을 위해 최선을 다하겠습니다</h3>
                 </div>
-                <div>
+              </Col>
+
+              <Col className="landing_text_section3">
+                <button
+                  className="landing_button ease_in_bottom_2"
+                  onClick={() => props.setBrowse(true)}
+                >
+                  SHOP NOW
+                </button>
+              </Col>
+            </Row>
+          </Col>
+
+          <Col lg={6}>
+            <div className="landing_image_container ease_in_right_1">
+              <Swiper
+                spaceBetween={500}
+                autoplay={{
+                  delay: 4500,
+                  disableOnInteraction: false,
+                }}
+                speed={1100}
+                modules={[Autoplay]}
+                loop={true}
+              >
+                <SwiperSlide>
                   <img
-                    src="/images/deco_cake.png"
+                    src="/images/landing_test_3.png"
+                    className="landing_image_background_removed ease_in_right_2"
                     alt=""
-                    className="deco_cake_image_right ease_in_bottom_1"
-                  />
-                </div> */}
-  
-          {/* <div>
-              <img
-                src="/images/deco_flower.png"
-                alt=""
-                className="deco_flower_img_left ease_in_bottom_1"
-              />
+                  ></img>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    src="/images/landing_test.png"
+                    className="landing_image_background_removed ease_in_right_2"
+                    alt=""
+                  ></img>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    src="/images/landing_test_2.png"
+                    className="landing_image_background_removed ease_in_right_2"
+                    alt=""
+                  ></img>
+                </SwiperSlide>
+              </Swiper>
             </div>
-            <div>
-              <img
-                src="/images/deco_flower.png"
-                alt=""
-                className="deco_flower_img_right ease_in_bottom_1"
-              />
-            </div> */}
-     </Container>
+          </Col>
+
+          {/* <div>
+                    <img
+                      src="/images/deco_cake.png"
+                      alt=""
+                      className="deco_cake_image_left ease_in_bottom_1"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      src="/images/deco_cake.png"
+                      alt=""
+                      className="deco_cake_image_right ease_in_bottom_1"
+                    />
+                  </div> */}
+
+          {/* <div>
+                <img
+                  src="/images/deco_flower.png"
+                  alt=""
+                  className="deco_flower_img_left ease_in_bottom_1"
+                />
+              </div>
+              <div>
+                <img
+                  src="/images/deco_flower.png"
+                  alt=""
+                  className="deco_flower_img_right ease_in_bottom_1"
+                />
+              </div> */}
+        </Row>
+      </Container>
     </>
   );
 };
