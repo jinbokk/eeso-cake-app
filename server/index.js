29bca23aaaab95a88a92fda753d77bb7cfa477ce
JINBOK LEE <eyelash1024@naver.com>
2022-12-03 16:45:47 +0900

    deploy test

--- server/index.js
+++ server/index.js
@@ -49,16 +49,16 @@ app.use("/temp", express.static("temp"));
 app.use(express.static(__dirname + "/public"));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  // All the javascript and css files will be read and served from this folder
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+// Set static folder
+// All the javascript and css files will be read and served from this folder
+app.use(express.static("client/build"));
 
-  // index.html for all page routes    html or routing and naviagtion
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
-}
+// index.html for all page routes    html or routing and naviagtion
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+});
+// }
 
 const port = process.env.PORT || 5000;
 
