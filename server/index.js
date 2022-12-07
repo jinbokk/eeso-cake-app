95f2d3f46d1a416834f092adf4d7a88a2c3a137c
JINBOK LEE <eyelash1024@naver.com>
2022-12-08 00:27:01 +0900

    file reorganization

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname + "/public"));
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server Listening on ${port}`);
