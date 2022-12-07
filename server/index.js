b0015b8c74ba84c18f2884fd6028128f969fc7d5
JINBOK LEE <eyelash1024@naver.com>
2022-12-07 20:42:44 +0900

    library update

--- server/index.js
+++ server/index.js
@@ -6,13 +6,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
-
-const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
 const mongoose = require("mongoose");
-// const { User } = require("./models/User");
-// const { Product } = require("./models/Product");
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -22,22 +18,10 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-// mongoose.connect(process.env.MONGO_URI);
-// const db = mongoose.connection;
-// db.on("error", (error) => console.log(error));
-// db.once("open", () => console.log(`Connected to mongoDB`));
-
 app.use(cors());
 
-//to not get any deprecation warning or error
-//support parsing of application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: true }));
-//to get json data
-// support parsing of application/json type post data
-app.use(bodyParser.json());
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/users", require("./routes/users"));
@@ -53,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
   // All the javascript and css files will be read and served from this folder
   app.use(express.static("client/build"));
 
-  // index.html for all page routes html or routing and naviagtion
+  // index.html for all page routes html
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
