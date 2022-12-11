eee989b3a20ad0fb58de59e6d74522e717e01293
JINBOK LEE <eyelash1024@naver.com>
2022-12-12 00:41:15 +0900

    fixing render error on refresh page

--- server/app.js
+++ server/app.js
@@ -72,12 +72,11 @@ app.use(function (err, req, res, next) {
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   // All the javascript and css files will be read and served from this folder
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
 
   // index.html for all page routes html
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
+  app.get('*', function (req, res){  
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html')) })
 }
 
 module.exports = app;
