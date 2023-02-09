c768004cd78ba5e2b469ab4ac8f260d177e86ac6
JINBOK LEE <eyelash1024@naver.com>
2023-02-09 21:24:21 +0900

    node-scheduler test

--- server/util/autoUpdateDB.js
+++ server/util/autoUpdateDB.js
@@ -1,11 +1,11 @@
 const { User } = require("../models/User");
 
 module.exports.autoUpdateDB = () => {
-  User.findOne({ name: "이진복" }, {}, (err, result) => {
+  User.findOne({ name: "이진복" }, (err, result) => {
     if (err) {
       return err;
     } else {
-      return result;
+      return result.data;
     }
   });
 };
