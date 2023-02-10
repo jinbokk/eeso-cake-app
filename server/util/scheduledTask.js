06aea95e26aae0dffc1c4fbe35adc2b5fe365bc7
JINBOK LEE <eyelash1024@naver.com>
2023-02-10 16:17:48 +0900

    scheduler test

--- server/util/scheduledTask.js
+++ server/util/scheduledTask.js
@@ -14,9 +14,9 @@ exports.update_order_status = () => {
       arrayFilters: [
         {
           "elem.status": "order_paid",
-          "elem.createdAt": new Date(
-            new Date().getTime() + KR_TIME_DIFF - oneDay
-          ), // 1일 전
+          "elem.createdAt": {
+            $gt: new Date(new Date().getTime() + KR_TIME_DIFF - oneDay),
+          }, // 1일 전
         },
       ],
     }
