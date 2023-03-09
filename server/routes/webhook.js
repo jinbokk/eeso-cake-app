c5aff85d9fe7d4a3689e40b91c75f835240af1fc
JINBOK LEE <eyelash1024@naver.com>
2023-03-09 17:20:23 +0900

    webhook test

--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -14,50 +14,9 @@ router.post("/portOne", async (req, res) => {
   const { imp_uid, merchant_uid } = req.body;
   const order_status = req.body.status;
 
-  if (order_status === "cancelled") {
-    try {
-      await User.findOneAndUpdate(
-        { history: { $elemMatch: { imp_uid: imp_uid } } },
-        // { _id: req.user._id },
-        {
-          $set: { "history.$[elem].status": "order_cancelled" },
-        },
-        { new: true, arrayFilters: [{ "elem.imp_uid": imp_uid }] }
-      );
-
-      return res.status(200).json({
-        status: "cancelled_confirm_success",
-        message: "관리자페이지 취소 DB 업데이트 성공",
-      });
-    } catch (error) {
-      return res.status(400).json({
-        status: "cancelled_confirm_error",
-        message: "관리자페이지 취소 DB 업데이트 실패",
-      });
-    }
-  }
-
-  ///////////             done
-
-  // 액세스 토큰(access token) 발급 받기
-  /* ...중략... */
-
-  // const form = new FormData();
-  // form.append("imp_key", process.env.IMP_API_KEY);
-  // form.append("imp_secret", process.env.IMP_API_SECRET);
-
-  // console.log("imp_key", process.env.IMP_API_KEY);
-  // console.log("imp_secret", process.env.IMP_API_SECRET);
-
-  // const getToken = await axios.post(
-  //   `https://api.iamport.kr/users/getToken`,
-  //   form
-  // );
-
   const access_token = await iamportGenerateAccessToken();
 
   // imp_uid로 포트원 서버에서 결제 정보 조회
-  /* ...중략... */
 
   const getPaymentData = await axios.get(
     `https://api.iamport.kr/payments/${imp_uid}`,
@@ -65,8 +24,6 @@ router.post("/portOne", async (req, res) => {
   );
 
   const paymentData = getPaymentData.data.response; // 조회한 결제 정보
-
-  console.log("paymentData:::", paymentData);
 
   //     // DB에서 결제되어야 하는 금액 조회
 
@@ -77,16 +34,13 @@ router.post("/portOne", async (req, res) => {
     }
   ).then((order) => order.history[0]);
 
-  console.log("order test::::", order);
-
   const amountToBePaid = order.amount; // 결제 되어야 하는 금액
 
-  console.log("amountToBePaid::::", amountToBePaid);
   // // 결제 검증하기
   const { amount } = paymentData;
   const paymentStatus = paymentData.status;
   // 결제금액 일치. 결제 된 금액 === 결제 되어야 하는 금액
-  if (amount === amountToBePaid) {
+  if (parseInt(amount) === parseInt(amountToBePaid)) {
     // DB에 결제 정보 저장
     // await Orders.findByIdAndUpdate(merchant_uid, { $set: paymentData });
     switch (paymentStatus) {
@@ -102,6 +56,28 @@ router.post("/portOne", async (req, res) => {
       //   });
       //   res.send({ status: "vbankIssued", message: "가상계좌 발급 성공" });
       //   break;
+      case "cancelled": // 결제 취소
+        try {
+          await User.findOneAndUpdate(
+            { history: { $elemMatch: { imp_uid: imp_uid } } },
+            // { _id: req.user._id },
+            {
+              $set: { "history.$[elem].status": "order_cancelled" },
+            },
+            { new: true, arrayFilters: [{ "elem.imp_uid": imp_uid }] }
+          );
+
+          return res.status(200).json({
+            status: "cancelled_confirm_success",
+            message: "관리자페이지 취소 DB 업데이트 성공",
+          });
+        } catch (error) {
+          return res.status(400).json({
+            status: "cancelled_confirm_error",
+            message: "관리자페이지 취소 DB 업데이트 실패",
+          });
+        }
+
       case "paid": // 결제 완료
         try {
           await User.findOneAndUpdate(
@@ -132,4 +108,4 @@ router.post("/portOne", async (req, res) => {
   }
 });
 
-module.exports = router;
+router.module.exports = router;
